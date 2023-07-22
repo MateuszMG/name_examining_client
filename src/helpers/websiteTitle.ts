9d26863a15d6f611f154de8b71b71ccd472175ac
Mateusz MG <MateuszMG.default@com.com>
2023-07-22 14:10:56 +0200

    fix: remove unnecessary return statement

--- src/helpers/websiteTitle.ts
+++ src/helpers/websiteTitle.ts
@@ -3,6 +3,4 @@ import { separateString } from './strings';
 export const setWebsiteTitle = (path: string) => {
   document.title =
     path === '/' ? 'Name examining' : separateString(path.slice(1));
-
-  return;
 };
